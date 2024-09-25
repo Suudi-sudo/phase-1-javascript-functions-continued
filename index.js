2eb5780b9843211e50fa9cad63f75c53edf05127
suudi abdisalan <abdisalansucdi@gmail.com>
2024-09-25 18:15:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("bathe my dog"));
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork('work from home'));
+
+
+function wrapAdjective(flair){
+    return function(adjective){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+const wrapWithStars = wrapAdjective('*');
+console.log(wrapWithStars('amazing'));
+
+const wrapWithL1s = wrapAdjective('||');
+console.log(wrapWithL1s('wonderful'));
+
